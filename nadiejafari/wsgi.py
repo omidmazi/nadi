a8da8b1f402c262654e17ab5733a561d9b05fc81
omidmazi <82062151+omidmazi@users.noreply.github.com>
2022-09-23 11:00:38 +0330

    Add files via upload

---
+++ nadiejafari/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nadiejafari project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nadiejafari.settings')
+
+application = get_wsgi_application()
